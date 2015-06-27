a4c33fe2106aeee62a6b39ae8e7fca2ed01cdc3c
joetinoco <jose.tinoco@gmail.com>
2015-06-27 12:16:14 -0400

    First API test

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 /*jshint node:true*/
 
 //------------------------------------------------------------------------------
-// node.js starter application for Bluemix
+// BLABLABRA INSIGHTS API
+//
+// Provides a "personality analysis" for text extracted from a region's tweets.
+// Uses IBM Watson's "Personality Insights" API.
+// 
+// This code was built upon node.js starter application for Bluemix.
+// 
 //------------------------------------------------------------------------------
 
 // This application uses express as it's web server
 // for more info, see: http://expressjs.com
 var express = require('express');
 
-// cfenv provides access to your Cloud Foundry environment
+// cfenv provides access to IBM's Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
@@ -16,10 +22,44 @@ var cfenv = require('cfenv');
 var app = express();
 
 // serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
+
+// =================================================
+// Serves the API functionality from the server root
+// =================================================
+app.get('/', function(req, res){
+	data = {}; // Return data will be in JSON format and stored here.
+
+	// Usage: API calls must include a query parameter, 'q'
+	if (req.query.q === undefined){
+		data['error'] = 'Parameters missing.';
+	} else {
+		// Invokes Watson's personality insights
+		var watson = require('watson-developer-cloud');
+		var personality_insights = watson.personality_insights({
+		  username: '6dd0d668-3d95-42ac-89c6-df0e12409098',
+		  password: 'LFB7Hdjw5jmF',
+		  version: 'v2'
+		});
+		console.log('I am here');
+		personality_insights.profile({ text: req.query.q },
+		  function (err, response) {
+		    if (err) {
+		  		data['error'] = err;
+		  		console.log(err);
+		    } else {
+		        data['response'] = JSON.stringify(response, null, 2);
+		        data['input'] = req.query.q;
+	        }
+		});
+	}
+	
+	// Finally, return the results of our hard work.
+	res.send(data);
+});
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, appEnv.bind, function() {
