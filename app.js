9dbd42118a5f7966c6d29ab71468fc02aa7c15ab
joetinoco <jose.tinoco@gmail.com>
2015-06-27 13:43:58 -0400

    Test mode added

--- app.js
+++ app.js
@@ -33,36 +33,46 @@ var appEnv = cfenv.getAppEnv();
 // =================================================
 app.get('/', function(req, res){
 	var data = {}; // Return data will be in JSON format and stored here.
+	res.set('Content-Type', 'text/plain');
 
 	// Usage: API calls must include a query parameter, 'q'
 	if (req.query.q === undefined){
 		data['error'] = 'Parameters missing.';
 		res.send(data);
 	} else {
-		// Invokes Watson's personality insights
-		var watson = require('watson-developer-cloud');
-		var personality_insights = watson.personality_insights({
-		  username: '6dd0d668-3d95-42ac-89c6-df0e12409098',
-		  password: 'LFB7Hdjw5jmF',
-		  version: 'v2'
-		});
-		personality_insights.profile({ text: req.query.q },
-		  function (err, response) {
-		    if (err) {
-		    	data['error'] = err;
-		    } else {
-		        data['response'] = response;
-		        data['input'] = req.query.q;
-	        }
-	        // Finally, return the results of our hard work.
-	        res.send(data);
-		});
+		if (req.query.q === 'TESTMODE'){
+			// This 'test mode' returns sample data for front-end development, saving API calls.
+			var fs = require('fs');
+			fs.readFile('./testdata.json', function (ferr, fdata) {
+			  if (ferr) throw ferr;
+			  res.send(fdata);
+			});
+
+		} else {
+			// Invokes Watson's personality insights
+			var watson = require('watson-developer-cloud');
+			var personality_insights = watson.personality_insights({
+			  username: '6dd0d668-3d95-42ac-89c6-df0e12409098',
+			  password: 'LFB7Hdjw5jmF',
+			  version: 'v2'
+			});
+			personality_insights.profile({ text: req.query.q },
+			  function (err, response) {
+			    if (err) {
+			    	data['error'] = err;
+			    } else {
+			        data['response'] = response;
+			        data['input'] = req.query.q;
+		        }
+		        // Finally, return the results of our hard work.
+		        res.send(data);
+			});
+		}
 	}
 });
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, appEnv.bind, function() {
-
 	// print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+  	console.log("server starting on " + appEnv.url);
 });
