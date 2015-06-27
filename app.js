909766dd5fc31b4f34ad83f1f5a864357d67777c
joetinoco <jose.tinoco@gmail.com>
2015-06-27 19:14:08 -0400

    HTTP header setup for CORS

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ var appEnv = cfenv.getAppEnv();
 app.get('/', function(req, res){
 	var data = {}; // Return data will be in JSON format and stored here.
 	res.set('Content-Type', 'text/plain');
+	res.set('Access-Control-Allow-Origin', 'blablabra.net, localhost');
 
 	// Usage: API calls must include a query parameter, 'q'
 	if (req.query.q === undefined){
