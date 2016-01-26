e90f88a2808546103b0b983d395d977ed448f71b
joetinoco <jose.tinoco@gmail.com>
2016-01-26 08:02:18 -0500

    CORS fix attempt, treats preflight requests properly

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 app.use(bodyParser.json()); // Prepare to parse POST data
-app.options('/', cors());
-app.use(cors());
+//app.options('/', cors());
+//app.use(cors());
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
@@ -34,18 +34,27 @@ var watsonAuthInfo = JSON.parse(fileSystem.readFileSync('./watsoncredentials.jso
 var watson = require('watson-developer-cloud');
 var personality_insights = watson.personality_insights(watsonAuthInfo.credentials);
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+	console.log('Request received: ' + req.method);
+	if (req.method == 'OPTIONS'){
+		res.sendStatus(200);
+	} else {
+		next();
+	}
+});
+
 // =================================================
 // Serves the API functionality from the server root
 // =================================================
-app.post('/', cors(), function(req, res, next){
-	res.set('Content-Type', 'text/plain');
-	res.set('Access-Control-Allow-Origin', '*');
+app.post('/', function(req, res, next){
+	//res.set('Content-Type', 'text/plain');
 	var query = req.body.query;
 	var data = {}; // Return data will be in JSON format and stored here.
+	console.log('Request payload: ' + query.length);
 
-	if (req.method == 'OPTIONS'){
-		res.sendStatus(200);
-	} else if (query === undefined){
+	if (query === undefined){
 		// Usage: API calls must include a JSON object with a "query" attribute
 		data['error'] = 'Parameters missing.';
 		res.send(data);
